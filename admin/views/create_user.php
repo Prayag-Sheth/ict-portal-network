89f848c7175afe8e980653d6aca06166923e8d4d
Prayag-Sheth <prayagsheth94@gmail.com>
2023-11-08 00:12:31 +0530

    LinkedIn Data fetched while user creation

--- admin/views/create_user.php
+++ admin/views/create_user.php
@@ -4,9 +4,8 @@
     <title>Create Student Profile</title>
     <!-- Add Bootstrap CSS (Place this before your custom CSS) -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <!-- Link your custom CSS file -->
-    <link rel="stylesheet" type="text/css" href="your-custom.css">
 </head>
+
 <body>
     <div class="container">
         <h1 class="mt-4">Create Student Profile</h1>
@@ -14,73 +13,77 @@
         <form action="add_student.php" method="post" onsubmit="return confirmAddUser()" class="mt-4">
             <div class="form-group">
                 <label for="name">Name:</label>
-                <input type="text" name="name" class="form-control" required>
+                <input type="text" name="name" id="name" class="form-control" required>
             </div>
 
             <div class="form-group">
                 <label for="email">Email:</label>
-                <input type="email" name="email" class="form-control" required>
+                <input type="email" name="email" id="email" class="form-control" required>
             </div>
 
+                     
             <div class="form-group">
-                <label for="linkedin_link">LinkedIn Profile:</label>
-                <input type="url" name="linkedin_link" class="form-control" required>
-            </div>
+            <label for="linkedin_link">LinkedIn Profile:</label>
+           
+            <input type="text" id="linkedin_link" class="form-control" required>
+          </div>
+           <button type="button" id="fetch-button" class="btn btn-primary">Fextch LinkedIn Data</button>
 
+          
             <div class="form-group">
                 <label for="skills">Skills:</label>
-                <textarea name="skills" rows="4" class="form-control" required></textarea>
+                <textarea name="skills" id="skills" rows="4" class="form-control" required></textarea>
             </div>
 
             <div class="form-group">
                 <label for="interests">Interests:</label>
-                <textarea name="interests" rows="4" class="form-control" required></textarea>
+                <textarea name="interests" id = "interests" rows="4" class="form-control" required></textarea>
             </div>
 
             <div class="form-group">
                 <label for="current_job_company">Current Job/Company:</label>
-                <input type="text" name="current_job_company" class="form-control" required>
+                <input type="text" name="current_job_company" id="current_job_company" class="form-control" required>
             </div>
 
             <div class="form-group">
                 <label for="domain_of_work">Domain of Work:</label>
-                <input type="text" name="domain_of_work" class="form-control" required>
+                <input type="text" name="domain_of_work" id="domain_of_work" class="form-control" required>
             </div>
 
-            <!-- New Fields -->
             <div class="form-group">
                 <label for="schools">Schools:</label>
-                <input type="text" name="schools" class="form-control" required>
+                <input type="text" name="schools" id="schools" class="form-control" required>
             </div>
 
             <div class="form-group">
                 <label for="universities">Universities:</label>
-                <input type="text" name="universities" class="form-control" required>
+                <input type="text" name="universities" id="universities" class="form-control" required>
             </div>
 
             <div class="form-group">
                 <label for="internship">Internship:</label>
-                <input type="text" name="internship" class="form-control" required>
+                <input type="text" name="internship" id="internship" class="form-control" required>
             </div>
 
             <div class="form-group">
                 <label for="jobs">Jobs:</label>
-                <input type="text" name="jobs" class="form-control" required>
+                <input type="text" name="jobs" id="jobs" class="form-control" required>
             </div>
             
             <!-- End of New Fields -->
-
+            
             <button type="submit" class="btn btn-primary">Create Profile</button>
         </form>
-
+        
         <a href="./../dashboard.php" class="mt-4">Back to Dashboard</a>
     </div>
-
+    
     <!-- Add Bootstrap JavaScript (Place this before your custom JavaScript) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-
+    
+    
     <script>
         function confirmAddUser() {
             var confirmation = confirm("Are you sure you want to add this student profile?");
@@ -90,6 +93,57 @@
                 return false; // Cancel form submission
             }
         }
+
+
+// JavaScript code
+$(document).ready(function() {
+  // Add a submit event listener to the input field
+  $("#fetch-button").click(function() {
+    // Prevent the default form submission behavior
+    event.preventDefault();
+
+    // Get the link from the input field
+    var link = $("#linkedin_link").val();
+    
+    // Make an Ajax request to the Python script route
+    $.ajax({
+      type: "GET",
+      url: "http://localhost:8000/my-first-api?link="+link,
+      data: {
+        link: link
+      },
+      success: function(response) {
+        // Parse the JSON response
+        // var profile_data = JSON.parse(response);
+
+        if(response.experiences.length > 0){
+            $("#current_job_company").val(response.experiences[0]['company']);
+            var company_name_list = []
+            for(i=0;i<response.experiences.length; i++){
+                company_name_list.push(response.experiences[i]['company'])
+            }
+            $("#jobs").val(company_name_list.join(", "))
+        }
+        if(response.interests.length > 0){
+            $("#interests").val(response.interests[0]);
+        }
+        if(response.education.length > 0){
+            $("#schools").val(response.education[0]['school']);
+            $("#universities").val(response.education[0]['school']);
+        }
+        $("#name").val(response.full_name);
+        $("#domain_of_work").val(response.occupation)
+        // Do something with the profile data
+      },
+      error: function(xhr, status, error) {
+        debugger;
+        // Handle the error
+        alert("Error fetching LinkedIn data.");
+      }
+    });
+  });
+});
+
     </script>
 </body>
 </html>
---
+++ assets/python/fetch_linkedin_data.py
@@ -0,0 +1,197 @@
+# python -m uvicorn fetch_linkedin_data:app --reload
+# def temp():
+import json
+# Load the JSON data
+  # json_data = '{"public_identifier": "john_doe","profile_pic_url": "https://example.com/profile_pic",  "full_name": "John Doe",  "skills": ["Skill1", "Skill2"],    "occupation": "Software Engineer",    "headline": "Full Stack Developer",    "summary": "A passionate developer with a love for coding.",   "country": "US","city": "New York"}'
+  # json_object = json.loads(json_data)
+  # return json_object
+
+from fastapi import FastAPI
+app = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# @app.get("/my-first-api")
+# def hello(link=None):
+#   json_data = '{"public_identifier": "john_doe","profile_pic_url": "https://example.com/profile_pic",  "full_name": "John Doe",  "skills": ["Skill1", "Skill2"],    "occupation": "Software Engineer",    "headline": "Full Stack Developer",    "summary": "A passionate developer with a love for coding.",   "country": "US","city": "New York"}'
+#   json_object = json.loads(json_data)
+#   return json_object
+    
+
+
+
+# Output:
+# ['Software Engineer', 30, 'John Doe']
+
+
+
+import requests
+api_endpoint = 'https://nubela.co/proxycurl/api/v2/linkedin'
+# linkedin_profile_url = 'hps://ttwww.linkedin.com/in/krunaltrivedi2601'
+api_key = 'iy4H4v176G5O_tBdx2z8wA'
+
+@app.get("/my-first-api")
+def hello(link=''):
+# -----------------------------------------------------------
+
+  response = {'public_identifier': 'prayag-sheth-75426716b',
+ 'profile_pic_url': 'https://media.licdn.com/dms/image/C4E03AQG0xPl4gyHZmQ/profile-displayphoto-shrink_800_800/0/1610258279544?e=1704931200&v=beta&t=D__XrquHSZ5zEra3zJAPv5xxQouxf_yD15ujTvSAX8M',
+ 'background_cover_image_url': None,
+ 'first_name': 'Prayag',
+ 'last_name': 'Sheth',
+ 'full_name': 'Prayag Sheth',
+ 'follower_count': 306,
+ 'occupation': 'Undergraduate Student at ICT - Marwadi University',
+ 'headline': 'Student at Marwadi University',
+ 'summary': None,
+ 'country': 'IN',
+ 'country_full_name': 'India',
+ 'city': 'Rajkot',
+ 'state': 'Gujarat',
+ 'experiences': [{'starts_at': {'day': 1, 'month': 6, 'year': 2021},
+   'ends_at': None,
+   'company': 'ICT - Marwadi University',
+   'company_linkedin_profile_url': 'https://www.linkedin.com/company/ict-marwadi-university/',
+   'title': 'Undergraduate Student',
+   'description': None,
+   'location': 'Rajkot, Gujarat, India',
+   'logo_url': 'https://media.licdn.com/dms/image/C560BAQH5v5AZ9ueuiA/company-logo_400_400/0/1590732223118?e=1707350400&v=beta&t=TuFmbwu0mmFM0e8NX-oA5BgNdRnU-a0cL197wIzMJsM'},
+  {'starts_at': {'day': 1, 'month': 10, 'year': 2018},
+   'ends_at': {'day': 30, 'month': 6, 'year': 2021},
+   'company': 'ICT - Marwadi University',
+   'company_linkedin_profile_url': 'https://www.linkedin.com/company/ict-marwadi-university/',
+   'title': 'Diploma Student',
+   'description': None,
+   'location': 'Rajkot, Gujarat, India',
+   'logo_url': 'https://media.licdn.com/dms/image/C560BAQH5v5AZ9ueuiA/company-logo_400_400/0/1590732223118?e=1707350400&v=beta&t=TuFmbwu0mmFM0e8NX-oA5BgNdRnU-a0cL197wIzMJsM'},
+  {'starts_at': {'day': 1, 'month': 7, 'year': 2023},
+   'ends_at': {'day': 30, 'month': 9, 'year': 2023},
+   'company': 'JAB Companies India',
+   'company_linkedin_profile_url': None,
+   'title': 'Software Developer Intern',
+   'description': '• Back-end development exposure using Java and Spring Boot.\n• Integrated front-end technologies (HTML, CSS) for user-friendly interfaces.\nContributed to database design and participated in code reviews.\n• Conducted testing in Postman and debugging to ensure code quality',
+   'location': 'Gandhinagar, Gujarat, India',
+   'logo_url': None},
+  {'starts_at': {'day': 1, 'month': 5, 'year': 2022},
+   'ends_at': {'day': 31, 'month': 7, 'year': 2022},
+   'company': 'DataFirm Tech',
+   'company_linkedin_profile_url': 'https://www.linkedin.com/company/datafirm-tech1808/',
+   'title': 'Machine Learning Intern',
+   'description': 'Worked with various algorithms such as RNN, LSTM, CNN for stock market price prediction',
+   'location': 'Ahmedabad, Gujarat, India',
+   'logo_url': 'https://media.licdn.com/dms/image/C4D0BAQHfO_L_cX6XCg/company-logo_400_400/0/1677078831617?e=1707350400&v=beta&t=deZdwmWP_7YsxKJz-vHDgb9BZ7MvMh7ipFuzzHFBxa8'}],
+ 'education': [{'starts_at': {'day': 1, 'month': 1, 'year': 2021},
+   'ends_at': {'day': 31, 'month': 1, 'year': 2024},
+   'field_of_study': 'Information and comunicationTechnology',
+   'degree_name': "Bachelor's degree",
+   'school': 'Marwadi University',
+   'school_linkedin_profile_url': None,
+   'description': None,
+   'logo_url': 'https://media.licdn.com/dms/image/D4D0BAQFYsn2oalYGjQ/company-logo_400_400/0/1661430244631/marwadiuniversity_logo?e=1707350400&v=beta&t=U-rqDr2jQMf_BAgQ3ASiDISNTChj2wKkf0mN7OuctJo',
+   'grade': None,
+   'activities_and_societies': None},
+  {'starts_at': {'day': 1, 'month': 1, 'year': 2018},
+   'ends_at': {'day': 31, 'month': 1, 'year': 2021},
+   'field_of_study': 'Information and Communication Technology',
+   'degree_name': 'Diploma of Education',
+   'school': 'Marwadi University',
+   'school_linkedin_profile_url': None,
+   'description': None,
+   'logo_url': 'https://media.licdn.com/dms/image/D4D0BAQFYsn2oalYGjQ/company-logo_400_400/0/1661430244631/marwadiuniversity_logo?e=1707350400&v=beta&t=U-rqDr2jQMf_BAgQ3ASiDISNTChj2wKkf0mN7OuctJo',
+   'grade': None,
+   'activities_and_societies': None}],
+ 'languages': [],
+ 'accomplishment_organisations': [],
+ 'accomplishment_publications': [],
+ 'accomplishment_honors_awards': [],
+ 'accomplishment_patents': [],
+ 'accomplishment_courses': [],
+ 'accomplishment_projects': [],
+ 'accomplishment_test_scores': [],
+ 'volunteer_work': [],
+ 'certifications': [],
+ 'connections': None,
+ 'people_also_viewed': [{'link': 'https://www.linkedin.com/in/meet-savaliya-88585b1b6',
+   'name': 'Meet Savaliya',
+   'summary': 'Student at ICT - Marwadi University',
+   'location': None},
+  {'link': 'https://www.linkedin.com/in/harsh-jolapara-a63b99205',
+   'name': 'Harsh Jolapara',
+   'summary': 'B.TECH IN INFORMATION COMMUNICATION TECHNOLOGY',
+   'location': None},
+  {'link': 'https://www.linkedin.com/in/jigar-pandya-b451251b6',
+   'name': 'Jigar Pandya',
+   'summary': 'Assistant Professor at Marwadi University',
+   'location': None},
+  {'link': 'https://www.linkedin.com/in/vasu-savjani-b67b2719a',
+   'name': 'Vasu Savjani',
+   'summary': 'Student at Marwadi University',
+   'location': None},
+  {'link': 'https://www.linkedin.com/in/keyur-asodariya-4950bb1b6',
+   'name': 'Keyur Asodariya',
+   'summary': 'Software Engineer at 63 Moons Technologies Limited',
+   'location': None},
+  {'link': 'https://www.linkedin.com/in/ravi-kumar-a540ab222',
+   'name': 'RAVI KUMAR',
+   'summary': 'Student | Information technology',
+   'location': None},
+  {'link': 'https://www.linkedin.com/in/tapan-khokhariya-52b294205',
+   'name': 'TAPAN KHOKHARIYA',
+   'summary': 'Bachelor of Technology - BTech at Marwadi University',
+   'location': None},
+  {'link': 'https://www.linkedin.com/in/dhanrajsinh-parmar-658160176',
+   'name': 'Dhanrajsinh Parmar',
+   'summary': 'Student at Marwadi University Currently pursuing B.Tech',
+   'location': None},
+  {'link': 'https://www.linkedin.com/in/naresh-jadeja-08150218',
+   'name': 'Naresh Jadeja',
+   'summary': 'Registrar at Marwadi University',
+   'location': None},
+  {'link': 'https://www.linkedin.com/in/upadhyay-rajan-a35962215',
+   'name': 'Upadhyay Rajan',
+   'summary': 'Software Developer',
+   'location': None}],
+ 'recommendations': [],
+ 'activities': [],
+ 'similarly_named_profiles': [],
+ 'articles': [],
+ 'groups': [],
+ 'skills': ['Python (Programming Language)',
+  'Machine Learning Algorithms',
+  'Spring Boot',
+  'MySQL',
+  'JavaScript',
+  'Java',
+  'SQL',
+  'English',
+  'Information Technology',
+  'Microsoft PowerPoint',
+  'HTML',
+  'PHP'],
+ 'inferred_salary': None,
+ 'gender': None,
+ 'birth_date': None,
+ 'industry': None,
+ 'extra': None,
+ 'interests': [],
+ 'personal_emails': [],
+ 'personal_numbers': []}
+  return response
+# -----------------------------------------------------------
+
+  # headers = {'Authorization': 'Bearer ' + api_key}
+
+  # if link != '':
+  #   response = requests.get(api_endpoint,
+  #                           params={'url': link,'skills': 'include'},
+  #                           headers=headers)
+  #   return response.json()
+  # else:
+  #   return {'error': 'please provide valid link'}
